10e388e5e0d8e84b465a517cbeba6fe5834e121c
Maicon Michelon <maicon.f.michelon@gmail.com>
2017-08-05 01:11:47 -0300

    tratando quando uma partida é desmarcada

--- processador_desempenho.rb
+++ processador_desempenho.rb
@@ -5,25 +5,25 @@ class ProcessadorDesempenho
 			desempenho_mandante = partida.mandante.desempenho_mandante
 			desempenho_visitante = partida.visitante.desempenho_visitante
 
-			desempenho_mandante.jogos+=1
-			desempenho_mandante.gols_feitos+= partida.gols_mandante
-			desempenho_mandante.gols_sofridos+= partida.gols_visitante
+			if partida.gols_mandante && partida.gols_visitante
 
-			desempenho_visitante.jogos+=1
-			desempenho_visitante.gols_feitos += partida.gols_visitante
-			desempenho_visitante.gols_sofridos += partida.gols_mandante
+				desempenho_mandante.jogos+=1
+				desempenho_mandante.gols_feitos+= partida.gols_mandante
+				desempenho_mandante.gols_sofridos+= partida.gols_visitante
 
-			if partida.gols_visitante.eql? partida.gols_mandante
-			   desempenho_mandante.pontos+=1;
-			   desempenho_visitante.pontos+=1;
-			elsif partida.gols_visitante < partida.gols_mandante
-			   desempenho_mandante.pontos+= 3
-			else
-			   desempenho_visitante.pontos+=3
-			end
+				desempenho_visitante.jogos+=1
+				desempenho_visitante.gols_feitos += partida.gols_visitante
+				desempenho_visitante.gols_sofridos += partida.gols_mandante
 
-			if partida.mandante.id.eql? 265 
-				puts desempenho_mandante.gols_sofridos
+				if partida.gols_visitante.eql? partida.gols_mandante
+				   desempenho_mandante.pontos+=1;
+				   desempenho_visitante.pontos+=1;
+				elsif partida.gols_visitante < partida.gols_mandante
+				   desempenho_mandante.pontos+= 3
+				else
+				   desempenho_visitante.pontos+=3
+				end
+				
 			end
 		end
 	end
